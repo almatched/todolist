bbae4e77f6dfdc6ce589c4d0c2b5fa93f2936c0f
daniil folco <folco1999@gmail.com>
2023-04-19 19:25:08 +0200

    modify bug causing items to not be deleted from Today list, change port from 3000 to a port set by environment, if there is none, port remains 3000

--- app.js
+++ app.js
@@ -119,7 +119,7 @@ app.post("/delete", function(req, res) {
   const listName = req.body.listName;
 
   if (listName === "Today") {
-    Item.findByIdAndRemove()
+    Item.findByIdAndRemove(itemID)
     .then(() => res.redirect("/"))
     .catch(err => console.log(err));
   } else {
@@ -129,10 +129,11 @@ app.post("/delete", function(req, res) {
   }
 });
 
-// if () {
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-// }
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(port, function() {
+  console.log("Server started on port" + port);
 });
